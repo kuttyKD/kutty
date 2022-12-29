6e98293c87f2a233eaaad37fedfec2f1a72edb2d
kuttyKD <121562427+kuttyKD@users.noreply.github.com>
2022-12-29 19:11:17 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-    const response = await fetch('https://codex-chatgpt-o33z.onrender.com/', {
+    const response = await fetch('https://kutty.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
