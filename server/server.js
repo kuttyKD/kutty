a83459c4cb7086fe1ba7cee2378494cad5b4f7bb
kuttyKD <121562427+kuttyKD@users.noreply.github.com>
2022-12-29 19:04:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:3251'))
+app.listen(3251, () => console.log('AI server started on http://localhost:3251'))
