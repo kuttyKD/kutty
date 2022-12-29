5dc3d704bd8f2a2be90569e9ec6b3617a4531374
kuttyKD <121562427+kuttyKD@users.noreply.github.com>
2022-12-29 19:02:52 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from KUTTY!'
+    message: 'Hello from CodeX!'
   })
-});
+})
 
 app.post('/', async (req, res) => {
   try {
@@ -37,12 +37,12 @@ app.post('/', async (req, res) => {
 
     res.status(200).send({
       bot: response.data.choices[0].text
-    })
+    });
 
   } catch (error) {
-    console.error(error)
-    res.status(500).send({error});
+    // console.error(process.env.OPENAI_API_KEY,"error")
+    res.status(500).send(error || 'Something went wrong');
   }
 })
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
